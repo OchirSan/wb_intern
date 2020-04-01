dd89c5563a41592e13b351d4243bd0e6fd88d486
OchirSan <39770608+OchirSan@users.noreply.github.com>
2020-04-01 23:15:38 +0300

    update main.go

--- cmd/visitor/main.go
+++ cmd/visitor/main.go
@@ -6,15 +6,16 @@ import (
 	"github.com/OchirSan/wb_intern/tree/visitor/pkg/factory"
 	"github.com/OchirSan/wb_intern/tree/visitor/pkg/house"
 	"github.com/OchirSan/wb_intern/tree/visitor/pkg/street"
+	"github.com/OchirSan/wb_intern/tree/visitor/pkg/visitor"
 )
 
 func main(){
-	house:= house.NewHouse("Lenina 23", 120, true)
-	factory:= factory.NewFactory("Lenina 12", false)
-	bank:= bank.NewBank("Lenina 117", true)
+	house := house.NewHouse("Lenina 23", 120, true)
+	factory := factory.NewFactory("Lenina 12", false)
+	bank := bank.NewBank("Lenina 117", true)
 	street1 := street.NewStreet("Lenina", house, factory, bank)
 
-	visitor := street.NewVisitor()
-	res := visitor.Accept(street1)
+	visitor1 := visitor.NewVisitor()
+	res := street1.Accept(visitor1)
 	fmt.Println(res)
 }
