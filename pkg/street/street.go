5ee5e1b5ca07321e6d8c3658862e9c3233807836
OchirSan <39770608+OchirSan@users.noreply.github.com>
2020-04-01 23:11:35 +0300

    update street.go

--- pkg/street/street.go
+++ pkg/street/street.go
@@ -1,60 +1,72 @@
 package street
 
-import "fmt"
-
 type Visitor interface {
-	Accept(streetInterface) string
-}
-
-type bankInterface interface {
-	VisitBank() string
-}
-
-type houseInterface interface {
-	VisitHouse() string
-}
-
-type factoryInterface interface {
-	VisitFactory() string
-}
-
-type streetInterface interface {
-	VisitStreet() string
-}
-
-type street struct {
-	name    string
-	house   houseInterface
-	bank    bankInterface
-	factory factoryInterface
+	VisitHouse(HouseInterface) string
+	VisitBank(BankInterface) string
+	VisitFactory(FactoryInterface) string
+	VisitStreet(StreetInterface) string
 }
 
 type visitor struct{}
 
-func (v *visitor) Accept(s streetInterface) string {
-	return s.VisitStreet()
+type HouseInterface interface {
+	GetName() string
+	GetFinancialCondition() int
+	GetPrudency() bool
 }
 
-// Visitor visit street
-func (s street) VisitStreet() string {
-	res := fmt.Sprintln("visiting street", s.name)
-	res += s.house.VisitHouse()
-	res += s.bank.VisitBank()
-	res += s.factory.VisitFactory()
-	return res
+type BankInterface interface {
+	GetName() string
+	GetIsHadIndurance() bool
+}
+
+type FactoryInterface interface {
+	GetName() string
+	GetIsHadDanger() bool
+}
+
+type StreetInterface interface {
+	GetName() string
+	GetHouse() HouseInterface
+	GetBank() BankInterface
+	GetFactory() FactoryInterface
+	Accept(v Visitor) string
+}
+
+type street struct {
+	name    string
+	house   HouseInterface
+	bank    BankInterface
+	factory FactoryInterface
+}
+
+// Accept visitor at street
+func (s street) Accept(v Visitor) string {
+	return v.VisitStreet(&s)
+}
+
+func (s *street) GetName() string {
+	return s.name
+}
+
+func (s *street) GetHouse() HouseInterface {
+	return s.house
+}
+
+func (s *street) GetBank() BankInterface {
+	return s.bank
+}
+
+func (s *street) GetFactory() FactoryInterface {
+	return s.factory
 }
 
 // Create new street
-func NewStreet(name string, house houseInterface, factory factoryInterface, bank bankInterface) streetInterface {
+func NewStreet(name string, house HouseInterface, factory FactoryInterface, bank BankInterface) StreetInterface {
 	return &street{
 		name:    name,
 		house:   house,
 		factory: factory,
 		bank:    bank,
 	}
-}
-
-// Create new visitor
-func NewVisitor() Visitor {
-	return &visitor{}
 }
