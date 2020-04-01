68dab0620084701b5853924c00fedce2f7a0ee9c
OchirSan <39770608+OchirSan@users.noreply.github.com>
2020-04-01 14:58:30 +0300

    добавил в структуру новое поле, переделал функцию

--- pkg/team/team.go
+++ pkg/team/team.go
@@ -3,45 +3,53 @@ package team
 import "fmt"
 
 type carryInterface interface {
-	KillEnemy(string);
+	KillEnemy()
 }
 
 type supportInterface interface {
-	Buff(string) int;
+	Buff() bool
 }
 
 type ruinerInterface interface {
-	LeaveTheGame(string);
+	LeaveTheGame()
 }
 
 type Team interface {
-	PlayTheGame()
+	PlayTheGame(moral int)
 }
 
 type team struct {
-	player1 carryInterface
-	player2 supportInterface
-	player3 ruinerInterface
+	player1        carryInterface
+	player2        supportInterface
+	player3        ruinerInterface
+	moralCondition int
 }
 
 // Describe the game
-func (t *team) PlayTheGame() {
-	//Define 0 or 1 random, 0 is a carry, 1 is a ruiner
-	var a int = t.player2.Buff("merlin")
-	if a == 0 {
-		t.player1.KillEnemy("artur")
+func (t *team) PlayTheGame(moral int) {
+	t.moralCondition = moral
+	// Define false or true, false is a carry, true is a ruiner
+	var a bool = t.player2.Buff()
+	if a == false {
+		t.moralCondition += 50
+	} else {
+		t.moralCondition -= 50
+	}
+	if t.moralCondition >= 100 {
+		t.player1.KillEnemy()
 		fmt.Println("команда выигрывает")
 	} else {
-		t.player3.LeaveTheGame("ron")
+		t.player3.LeaveTheGame()
 		fmt.Println("команда проигрывает")
 	}
 }
 
-//Creates new team
+// Creates new team
 func NewTeam(player1 carryInterface, player2 supportInterface, player3 ruinerInterface) Team {
 	return &team{
-		player1:   player1,
-		player2:   player2,
-		player3:   player3,
+		player1:        player1,
+		player2:        player2,
+		player3:        player3,
+		moralCondition: 0,
 	}
 }
