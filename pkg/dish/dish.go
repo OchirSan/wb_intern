027db6c8aa48280d2da52e6b15fe5a0a1796af5d
OchirSan <39770608+OchirSan@users.noreply.github.com>
2020-04-06 16:29:06 +0300

    update dish.go
    add weightMeat, weightGarnish and method IsItDish() string

--- pkg/dish/dish.go
+++ pkg/dish/dish.go
@@ -6,19 +6,42 @@ type Dish interface {
 	SetMeat(string)
 	SetGarnish(string)
 	ShowDish()
+	GetMeat() int
+	GetGarnish() int
+	IsItDish() string
 }
 
 type dish struct {
-	meat    string
-	garnish string
+	meat          string
+	garnish       string
+	weightMeat    int
+	weightGarnish int
 }
 
 func (d *dish) SetMeat(s string) {
 	d.meat = s
 }
 
+func (d *dish) IsItDish() string {
+	if d.GetMeat() > 300 || d.GetGarnish() > 400 {
+		res := fmt.Sprintln("too much meat or garnish")
+		return res
+	} else {
+		res := fmt.Sprintln("Its, Ok, we can cook it")
+		return res
+	}
+}
+
 func (d *dish) SetGarnish(s string) {
 	d.garnish = s
+}
+
+func (d *dish) GetMeat() int {
+	return d.weightMeat
+}
+
+func (d *dish) GetGarnish() int {
+	return d.weightGarnish
 }
 
 // Show Dish
@@ -28,6 +51,9 @@ func (d *dish) ShowDish() {
 }
 
 // Create new dish
-func NewDish() Dish {
-	return &dish{}
+func NewDish(weightMeat int, weightGarnish int) Dish {
+	return &dish{
+		weightMeat:    weightMeat,
+		weightGarnish: weightGarnish,
+	}
 }
