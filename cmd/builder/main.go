027d8317d23aa3f4ee9814826ad48b0724712ffd
OchirSan <39770608+OchirSan@users.noreply.github.com>
2020-04-06 16:43:49 +0300

    update main.go

--- cmd/builder/main.go
+++ cmd/builder/main.go
@@ -8,14 +8,22 @@ import (
 )
 
 func main() {
-	dish1 := dish.NewDish()
-	dish2 := dish.NewDish()
-	a := pastaCutlets.NewPastaCutlets(dish1)
-	b := pilaf.NewPilaf(dish2)
-	director := builder.NewDirector(a)
-	director.BuildDish()
-	director1 := builder.NewDirector(b)
-	director1.BuildDish()
-	a.ShowDish()
-	b.ShowDish()
+	result := "too much meat or garnish"
+	dish1 := dish.NewDish(200, 150)
+	dish2 := dish.NewDish(250, 200)
+	res1 := dish1.IsItDish()
+	res2 := dish2.IsItDish()
+	if res1 != result {
+		a := pasta_cutlets.NewPastaCutlets(dish1)
+		director := builder.NewDirector(a)
+		director.BuildDish()
+		a.ShowDish()
+
+	}
+	if res2 != result {
+		b := pilaf.NewPilaf(dish2)
+		director1 := builder.NewDirector(b)
+		director1.BuildDish()
+		b.ShowDish()
+	}
 }
