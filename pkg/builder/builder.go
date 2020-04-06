c9a4036ea71d752bada40b5a92e1157d351583af
OchirSan <39770608+OchirSan@users.noreply.github.com>
2020-04-06 16:40:10 +0300

    add comments

--- pkg/builder/builder.go
+++ pkg/builder/builder.go
@@ -14,12 +14,13 @@ type director struct {
 	builder dishBuilder
 }
 
+// Concrete builder build new dish
 func (d *director) BuildDish() {
 	d.builder.FryMeat()
 	d.builder.MakeSideDish()
 }
 
-// Create new director
+// Create new director, whose leads the builder
 func NewDirector(builder dishBuilder) Director {
 	return &director{
 		builder: builder,
